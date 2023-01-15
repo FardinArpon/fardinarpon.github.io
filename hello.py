135b809f9576a121fa311b9e7aeb0adf017ad1df
Fardin Amin Arpon <fardinaminarpon@gmail.com>
2023-01-16 03:21:24 +0600

    cide

--- hello.py
+++ hello.py
@@ -1,12 +1,8 @@
 # Hello py
-print("Hello CiCd")
-print("Hello Fardin")
-print("Hello Tuhin vai")
-print("Hello shamim vai")
 <<<<<<< HEAD
 print("Hello Fardin Amin Arpon")
 
-print("Hello Fardin Amin Arpon")
+print("DevOps CICD")
 =======
 print("Hello Fardin")
 >>>>>>> a1f4e21ba86f26788ceafd0788c2b629b89da1ca
